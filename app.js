1fbb4caa7bd2b45774aea63bbd54d28e5b9da906
praveshtiwari150 <praveshtiwari150@users.noreply.github.com>
2022-10-03 19:32:56 +0530

    weather report

---
+++ app.js
@@ -0,0 +1,44 @@
+const api_key= '3265874a2c77ae4a04bb96236a642d2f';
+
+const form = document.querySelector("form")
+const weather = document.querySelector("#weather");
+const search = document.querySelector("#search");
+
+// const api= `https://api.openweathermap.org/data/2.5/weather?`;
+
+// const img_url = `https://openweathermap.org/img/wn/$data.weather[0].icon}@2x.png`;
+
+const getWeather = async(city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return showWeather(data);
+}
+
+const showWeather = (data) =>{
+    if(data.cod == "404"){
+        weather.innerHTML = `<h2>City Not Found</h2>`;
+        return;
+    }
+    console.log(data);
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+        </div>
+        <div>
+
+            <h2>Temperature : ${data.main.temp} °C</h2>
+            <h4>${data.weather[0].main}</h4>
+            <h2>Wind speed : ${data.wind.speed}</h2>
+            <h2>Country : ${data.sys.country}</h2>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event){
+        getWeather(search.value);
+        event.preventDefault();
+    }
+)
